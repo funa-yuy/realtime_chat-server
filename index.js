2626bfa4d4290b5db79069ff4ef252832c6429b4
funa <orz.home35@gmail.com>
2025-05-23 21:22:58 +0900

    🛠️ デプロイようにかえた

--- index.js
+++ index.js
@@ -7,7 +7,8 @@ const app = express();
 const server = createServer(app);
 const io = new Server(server, {
 	cors: {
-		origin: "http://localhost:3001", // Next.jsのデフォルトポート
+		//Vercelでデプロイした時のURLか、Next.jsのデフォルトポート
+		origin: process.env.CLIENT_URL || "http://localhost:3001", // Next.jsのデフォルトポート
 		methods: ["GET", "POST"]
 	}
 });
